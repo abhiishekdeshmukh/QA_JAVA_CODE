bdc295571c23304fff0357462c6416b467bf43a3
Abhishek <Abhishek@DESKTOP-4NQFNEB>
2021-09-15 10:51:40 +0530

    Updated fibo

--- 14thSeptPractical/src/com/prac/Fibonacci.java
+++ 14thSeptPractical/src/com/prac/Fibonacci.java
@@ -4,13 +4,17 @@ public class Fibonacci
 {
 	public static void main(String[] args) 
 	{
-		int num1, num2, num3, fibo = 0;
-		int input = 100;
-		for(int i = 1; i <= input; i++)
+		int num1 = 0, num2 = 1, num3, i;
+		System.out.println("Fibonacci Series from 1 to 100 is: ");
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		for(i=2; i<=100; i++)
 		{
-			System.out.println("Fibonacci Series is "+fibo);
+			num3 = (num1 + num2);
+			System.out.println(+num3);
+			num1 = num2;
+			num2 = num3;
 		}
-		
-		
 	}
 }
