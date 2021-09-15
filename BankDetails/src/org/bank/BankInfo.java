18aea9019e5ddfd9a488b152a7ced6f64978ebe4
Abhishek <Abhishek@DESKTOP-4NQFNEB>
2021-09-15 11:05:57 +0530

    Assignment 2

---
+++ BankDetails/src/org/bank/BankInfo.java
@@ -0,0 +1,22 @@
+package org.bank;
+
+public class BankInfo extends AxisBank
+{
+	public void saving() 
+	{
+
+	}
+	
+	public void fixed() 
+	{
+
+	}
+	public static void main(String[] args) 
+	{
+		BankInfo bi = new BankInfo();
+		bi.saving();
+		bi.fixed();
+		
+		bi.deposit();
+	}
+}
